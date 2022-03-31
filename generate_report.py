92655c5c9f193cd8fbb1999611e0104397cf366f
pamelaxu213 <pamela.xu@mail.utoronto.ca>
2022-03-31 10:59:18 -0400

    filter by #mode

--- generate_report.py
+++ generate_report.py
@@ -131,7 +131,7 @@ def main(report):
             "UCE_200bp",
         ]
     ]
-    report.sort_values(by = "#mode", ascending = True)
+    report = report.sort_values(by=["#mode"] )
     report.to_csv(
         "test.txt",
         index=False,
